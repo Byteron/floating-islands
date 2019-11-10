af293270884b0031bd1458cd7c31453443060144
Aaron Winter <winter.aaron93@gmail.com>
2019-11-10 14:35:45 +0100

    added popuplabel

--- source/construction/Construction.gd
+++ source/construction/Construction.gd
@@ -32,15 +32,23 @@ func mine() -> void:
 	if tile.has_resources():
 		var mined : int = tile.mine(miner_amount)
 		get_tree().call_group("Player", "add_resources", mined)
+		_make_popup(mined)
 	else:
 		for n_tile in tile.neighbors:
 			if n_tile.has_resources():
 				var mined : int = n_tile.mine(miner_amount)
 				get_tree().call_group("Player", "add_resources", mined)
+				_make_popup(mined)
 				return
 
 		mine_timer.stop()
 
+func _make_popup(value: int) -> void:
+	var popup := PopupLabel.instance() as PopupLabel
+	popup.text = "+%d" % value
+	popup.color = Color("00FF00")
+	popup.rect_global_position = global_position
+	get_tree().current_scene.add_child(popup)
 
 func _on_MineTimer_timeout() -> void:
 	mine()
