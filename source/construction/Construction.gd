a31d898703373fadaf02cb77814904a17d7aa652
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-11-12 11:27:18 +0100

    [fix] Clean warnings

--- source/construction/Construction.gd
+++ source/construction/Construction.gd
@@ -1,12 +1,17 @@
 extends Node2D
 class_name Construction
 
+# warning-ignore:unused_class_variable
 var texture_active : Texture = null
+# warning-ignore:unused_class_variable
 var texture_inactive : Texture = null
 
+# warning-ignore:unused_class_variable
 var miner_radius := 0
+# warning-ignore:unused_class_variable
 var miner_amount := 0
 
+# warning-ignore:unused_class_variable
 var is_miner := false
 var tiles: Array = [] setget , get_tiles
 var data: ConstructionData				# Construction data
