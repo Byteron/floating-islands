1963c87368c08ed8eb8855123a14e42dd2cbc503
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-11-12 11:26:36 +0100

    [add] Multiple resources

---
+++ source/resource/ResourceData.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name ResourceData
+
+# warning-ignore:unused_class_variable
+export var id := ""
+# warning-ignore:unused_class_variable
+export var name := ""
+# warning-ignore:unused_class_variable
+export var icon : Texture = null
