4031545b3c61158aa70cfc74e4f58d98a684f6f2
Aaron Winter <winter.aaron93@gmail.com>
2019-11-15 18:18:55 +0100

    menu tweens

--- source/menu/TitleScreen.gd
+++ source/menu/TitleScreen.gd
@@ -2,13 +2,26 @@ extends Control
 
 onready var center := get_viewport_rect().size / 2
 
+onready var tween := $Tween as Tween
+
 onready var stars1 := $BG/Stars1
 onready var stars2 := $BG/Stars2
 onready var earth := $BG/Earth
 
-func _ready() -> void:
-	Music.play_song("Loop")
+onready var cover := $Cover/ColorRect
+onready var title := $Title
+onready var menu := $CenterContainer
 
+func _ready() -> void:
+	Music.play_song("Loop", 2.0)
+
+	title.modulate.a = 0
+	menu.modulate.a = 0
+
+	var __ = tween.interpolate_property(cover, "modulate:a", 1, 0, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	__ = tween.interpolate_property(title, "modulate:a", 0, 1, 0.3, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 1)
+	__ = tween.interpolate_property(menu, "modulate:a", 0, 1, 0.3, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 1.3)
+	__ = tween.start()
 
 func _process(delta: float) -> void:
 	var offset = get_global_mouse_position() - center
